911fd05cd1eae69781fb967408bd45faf853c747
Big Star <137803468+mightstar@users.noreply.github.com>
2024-05-20 12:54:11 -0700

    add

--- JavaScript-animation/script.js
+++ JavaScript-animation/script.js
@@ -1,3 +1,37 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const image1 = document.querySelector(".image1");
+//   const imageContainer = document.querySelector(".image-container");
+//   const image2Container = document.querySelector(".image2-container");
+//   const container = document.querySelector(".container");
+
+//   const scrollRatio = 1 / 3;
+
+//   container.style.height = `calc(100vh + ${
+//     image1.offsetHeight / scrollRatio
+//   }px)`;
+//   const startOffset = 0;
+//   const endOffset = startOffset - image1.offsetHeight / scrollRatio;
+
+//   window.addEventListener("scroll", () => {
+//     const containerTop = container.getBoundingClientRect().top;
+//     image2Container.style.height =
+//       Math.max(
+//         0,
+//         image1.offsetHeight - (startOffset - containerTop) * scrollRatio
+//       ) + "px";
+//     if (containerTop <= startOffset && containerTop >= endOffset) {
+//       imageContainer.style.position = "fixed";
+//       imageContainer.style.top = startOffset + "px";
+//     } else if (containerTop > startOffset) {
+//       imageContainer.style.position = "relative";
+//       imageContainer.style.top = 0 + "px";
+//     } else if (containerTop <= endOffset) {
+//       imageContainer.style.position = "relative";
+//       imageContainer.style.top = image1.offsetHeight / scrollRatio + "px";
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const image1 = document.querySelector(".image1");
   const imageContainer = document.querySelector(".image-container");
@@ -12,6 +46,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const startOffset = 0;
   const endOffset = startOffset - image1.offsetHeight / scrollRatio;
 
+  let isSafari =
+    /^((?!chrome|android).)*safari/i.test(navigator.userAgent) ||
+    /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  console.log('Safari', isSafari);
+
   window.addEventListener("scroll", () => {
     const containerTop = container.getBoundingClientRect().top;
     image2Container.style.height =
@@ -19,15 +59,31 @@ document.addEventListener("DOMContentLoaded", () => {
         0,
         image1.offsetHeight - (startOffset - containerTop) * scrollRatio
       ) + "px";
-    if (containerTop <= startOffset && containerTop >= endOffset) {
-      imageContainer.style.position = "fixed";
-      imageContainer.style.top = startOffset + "px";
-    } else if (containerTop > startOffset) {
-      imageContainer.style.position = "relative";
-      imageContainer.style.top = 0 + "px";
-    } else if (containerTop <= endOffset) {
-      imageContainer.style.position = "relative";
-      imageContainer.style.top = image1.offsetHeight / scrollRatio + "px";
+    if (isSafari) {
+      if (containerTop <= startOffset && containerTop >= endOffset) {
+        imageContainer.style.position = "fixed";
+        imageContainer.style.top = startOffset + "px";
+      } else if (containerTop > startOffset) {
+        imageContainer.style.position = "absolute";
+        imageContainer.style.top = 0 + "px";
+      } else if (containerTop <= endOffset) {
+        imageContainer.style.position = "absolute";
+        imageContainer.style.top =
+          image1.offsetHeight / scrollRatio + "px";
+      }
+    } else {
+      if (containerTop <= startOffset && containerTop >= endOffset) {
+        imageContainer.style.position = "fixed";
+        imageContainer.style.top = startOffset + "px";
+      } else if (containerTop > startOffset) {
+        imageContainer.style.position = "relative";
+        imageContainer.style.top = 0 + "px";
+      } else if (containerTop <= endOffset) {
+        imageContainer.style.position = "relative";
+        imageContainer.style.top =
+          image1.offsetHeight / scrollRatio + "px";
+      }
     }
   });
 });
+// </script>
